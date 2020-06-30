6b4eac4829cddc3a3818ea37cd5b3e4f6eb661d6
Christopher Moore <moorec2@findlay.edu>
2020-06-30 14:20:29 -0400

    change icons and other updates

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,20 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  'publicPath':'/arete'
+  'publicPath':'/arete',
+  pwa: {
+    name: 'Arete',
+    themeColor: '#114474',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'src/service-worker.js',
+      // ...other Workbox options...
+    }
+  }
 }
